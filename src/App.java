3e8ccecb208d46fac461f98e8055c8490cfaef51
rizzarivincent <rizzarivincent@gmail.com>
2021-01-05 19:11:31 -0500

    Ripped out a bunch of old Java Swing code; time to relearn!

---
+++ src/App.java
@@ -0,0 +1,17 @@
+
+public class App implements Runnable {
+
+
+    public App() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new App();
+    }
+}
